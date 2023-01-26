97cfa69bdf095172b914c3aefa69e6546561334a
DJJuampi <63322323+DJJuampi@users.noreply.github.com>
2023-01-26 18:48:56 -0300

    v0.5
    Added points and a font

--- main.cpp
+++ main.cpp
@@ -1,20 +1,31 @@
 
 #include "SFML/Graphics.hpp"
 #include <iostream>
+#include <sstream>
+#include <string>
 
+template <typename T>
+std::string toString(T arg)
+{
+    std::stringstream ss;
+    ss << arg;
+    return ss.str();
+}
 using namespace sf;
 using namespace std;
 class player {
 public:
-    int Points;
-    int Jumps;
-    bool Attack = false;
+    int Points = 0;
+
+    bool AttackTop = false;
+    bool AttackBottom = false;
 };
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1280, 720), "Slime Runner");
     window.setFramerateLimit(60);
 //Player sprite
+   
     float JumpVelocity = -10;
     bool AuthJump = false;
     bool descent = false;
@@ -26,6 +37,7 @@ int main()
         return 0;
     }
     sf::Sprite playerSprite;
+
     playerSprite.setTexture(playerTexture);
     playerSprite.scale(sf::Vector2f(0.2, 0.2));
     sf::Vector2f playerPosition(100, 620);
@@ -47,7 +59,7 @@ int main()
 
     enemy1Sprite.setPosition(enem1Position);
     
-   
+    player player1;
 
 
     while (window.isOpen())
@@ -94,15 +106,34 @@ int main()
 
         enem1Position.x += xVelocity;
         enemy1Sprite.setPosition(enem1Position);
+        
         //collision
         
         if (playerSprite.getGlobalBounds().intersects(enemy1Sprite.getGlobalBounds())) {
             return 0;
         }
 
+        //Fonts and text
+        sf::Font font;
+        if (!font.loadFromFile("assets/font.ttf")) {
+            std::cout << "Could not load font";
+            return 0;
+        }
+       
+        sf::Text text;
+        sf::Text Points(toString<int>(player1.Points), font);
+
+        text.setFont(font);
+        text.setString("Puntaje: ");
+        Points.setPosition(1, 28);
+
+        //Point gain
+        player1.Points = player1.Points + 1;
 
         //render
         window.clear();
+        window.draw(text);
+        window.draw(Points);
         window.draw(playerSprite);
         window.draw(enemy1Sprite);
         window.display();
